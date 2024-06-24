3824c7666c7c89e781faab1ea1d0d3f75264068d
SudeerK <sudeercc.23@gmail.com>
2024-06-24 14:45:19 +0530

    install catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,9 @@ echo "Value of ID is : $ID"
 echo "Script Name is : $0"
 TimeStamp=$(date +%F-%H-%M-%S)
 echo "The script execution started at : $TimeStamp"
-LogFile
+LogFile="/tmp/$0-$TimeStamp.log"
+echo "LogFile name is :$LogFile"
+
 if [ $ID -ne 0 ]
 then 
 echo "Error:: Not a root user"
