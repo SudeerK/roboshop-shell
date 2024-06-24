803e3e96704ea19102db1316c4b82e16d58a9c4f
SudeerK <sudeercc.23@gmail.com>
2024-06-24 14:48:27 +0530

    install catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -8,9 +8,22 @@ echo "The script execution started at : $TimeStamp"
 LogFile="/tmp/$0-$TimeStamp.log"
 echo "LogFile name is :$LogFile"
 
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo "$1 is unsuccessful"
+    else
+    echo "$2 is successful"
+}
 if [ $ID -ne 0 ]
 then 
 echo "Error:: Not a root user"
 else 
 echo "Logged in as root user"
 fi
+
+dnf module disable nodejs -y
+VALIDATE $? "NodeJS is disabled"
+dnf module enable nodejs:18 -y
+VALIDATE $? "NodeJS 18 is enabled"
